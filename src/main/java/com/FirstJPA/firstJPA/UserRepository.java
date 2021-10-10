89b48fde36fd1e2aae96972e5aab8d55f8d18ce9
Suvrat <suvrat72000@gmail.com>
2021-10-10 19:07:31 +0530

    Project to connect Spring Boot application to the MySql database using JPA

---
+++ src/main/java/com/FirstJPA/firstJPA/UserRepository.java
@@ -0,0 +1,7 @@
+package com.FirstJPA.firstJPA;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Integer>{
+
+}
---
+++ src/test/java/com/FirstJPA/firstJPA/FirstJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FirstJPA.firstJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
